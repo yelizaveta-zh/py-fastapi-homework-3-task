9f69f82e5e9591eaccbc92f2aa9475f1ff5b2746
Lisa <zhyganiuk.elizaveta@gmail.com>
2025-03-18 11:31:06 +0200

    Solution

--- src/routes/accounts.py
+++ src/routes/accounts.py
@@ -18,8 +18,34 @@ from database import (
     RefreshTokenModel
 )
 from exceptions import BaseSecurityError
+from schemas.accounts import UserCreate
 from security.interfaces import JWTAuthManagerInterface
+from security.passwords import hash_password
+from security.utils import generate_secure_token
 
 router = APIRouter()
 
-# Write your code here
+
+@router.post("/register/", status_code=status.HTTP_201_CREATED)
+def register_user(user_data: UserCreate, db: Session = Depends(get_db)):
+    try:
+        existing_user = db.execute(select(UserModel).filter(UserModel.email == user_data.email)).scalar_one_or_none()
+        if existing_user:
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                                detail=f"A user with this email {user_data.email} already exists.")
+
+        hashed_password = hash_password(user_data.password)
+
+        new_user = UserModel(email=user_data.email, password=hashed_password, group=UserGroupEnum.USER)
+        db.add(new_user)
+        db.commit()
+        db.refresh(new_user)
+
+        activation_token = generate_secure_token()
+
+        return {"id": new_user.id, "email": new_user.email}
+
+    except SQLAlchemyError:
+        db.rollback()
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail="An error occurred during user creation.")
--- src/schemas/accounts.py
+++ src/schemas/accounts.py
@@ -3,4 +3,18 @@ from pydantic import BaseModel, EmailStr, field_validator
 from database import accounts_validators
 
 
-# Write your code here
+class UserBase(BaseModel):
+    email: EmailStr
+
+class UserCreate(UserBase):
+    password: str
+
+class UserRead(UserBase):
+    id: int
+
+    class Config:
+        from_attributes = True
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
